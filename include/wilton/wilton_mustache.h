d04a2f226f83f7d3751c6dd260645827e05e6721
alex <alex@staticlibs.net>
2018-04-05 23:38:24 +0100

    moved from server module

---
+++ include/wilton/wilton_mustache.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2018, alex at staticlibs.net
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef WILTON_MUSTACHE_H
+#define WILTON_MUSTACHE_H
+
+#include "wilton/wilton.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// todo
+
+char* wilton_mustache_render(
+        const char* template_text,
+        int template_text_len,
+        const char* values_json,
+        int values_json_len,
+        char** output_text_out,
+        int* output_text_len_out);
+
+char* wilton_mustache_render_file(
+        const char* template_file_path,
+        int template_file_path_len,
+        const char* values_json,
+        int values_json_len,
+        char** output_text_out,
+        int* output_text_len_out);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* WILTON_MUSTACHE_H */
+
